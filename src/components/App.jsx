47a63d3eedd62e61895e5d9f481616f5c244e279
Linda Wang <lindawang256@gmail.com>
2018-08-16 13:29:16 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <h1>This is my App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
